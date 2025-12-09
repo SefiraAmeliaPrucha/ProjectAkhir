a62acf11bea9c1369103a5cf387fd000fae48ccb
Prucha <sefiraameliaprucha@gmail.com>
2025-12-09 19:19:39 +0700

    nambahin bola, skor, dan simpenSkorKeFile

--- ProjekAkhir.cpp
+++ ProjekAkhir.cpp
@@ -18,6 +18,27 @@ int pKananY = TINGGI / 2 - 2;
 int pKananTinggi = 7;
 char pKananSimbol = '#';
 
+int bolaX = LEBAR / 2;
+int bolaY = TINGGI / 2;
+int arahBolaX = 1; // arah horizontal
+int arahBolaY = -1; // arah vertikal        
+char bolaSimbol = '*';
+
+int skorKiri = 0;
+int skorKanan = 0;
+int riwayatSkor[10];
+int skor = 0;
+
+void simpenSkorKeFile()
+{
+    ofstream file("RiwayatSkor.txt", ios::app);
+    if (file.is_open()){
+        file << "Skor Kiri: " << skorKiri << "| Skor Kanan: " << skorKanan << endl;
+        file.close();
+    }
+}
+
+
 
 int main() {
     
